13a2535c06ed7b857f835453c4a2d021aef3786c
01111111111111111111111111111111 <32463675+01111111111111111111111111111111@users.noreply.github.com>
2017-10-03 10:54:21 -0400

    1st iteration/rough draft BiggerNumber.java, looking into math method to compared two ints/primitives

--- week1-011.BiggerNumber/src/BiggerNumber.java
+++ week1-011.BiggerNumber/src/BiggerNumber.java
@@ -1,11 +1,43 @@
+/* Directions:
+Create a program that asks the user for two integers and then prints the larger of the two.
 
-import java.util.Scanner;
+Tip: When you write Math.
+(that is, Math followed by a dot) in NetBeans, it shows you a bunch of available mathematical calculations.
+For example, Math.cos(10) calculates the cosine of the number 10. Try to find a suitable tool in Math to complete this exercise!
+If you cannot find anything suitable or do not know how to complete the exercise, skip the exercise for now.
+We will return to the matter later on.
 
-public class BiggerNumber {
+Desired output:
+________________________
+Type a number: 20
+Type another number: 14
 
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+The bigger number of the two numbers given was: 20
+ */
 
-        // Implement your program here. Remember to ask the input from user
+import java.util.Scanner; //import Scanner class to call later and parse user input (primitives like double, int, string)
+
+public class BiggerNumber { //name of program same as .java, class
+
+    public static void main (String [] args) { //main method where majority of program will be written under
+
+        Scanner reader = new Scanner (System.in); //calls for new Scanner within main method
+
+        System.out.print("Type a number: "); //1st number to be received set up comparison--user string/prompt asking for input
+
+        int firstNum = Integer.parseInt(reader.nextLine()); //uses .nextLine to read input by user, accepts integer
+
+        System.out.print("Type another number: "); //similar set up as firstNum; now compare 2 integers to each other
+
+        int secondNum = Integer.parseInteger(reader.nextLine());
+
+        /*compare function here; comparing firstNum and secondNum
+        need to look up a math method to compare
+        maybe  java.lang.Math.max()?
+        */
+
+        System.out.print("The bigger number of the two numbers give was: " + );//whateverComparedVarOutput after string
     }
+
+
 }
