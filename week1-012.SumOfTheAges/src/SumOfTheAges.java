f3aa0800b4318e7ed67a7791cef32d7f5831feea
01111111111111111111111111111111 <32463675+01111111111111111111111111111111@users.noreply.github.com>
2017-10-03 12:19:33 -0400

    1st! 4 scanner vars to parse concatenation of int and strings to sum two people's age

--- week1-012.SumOfTheAges/src/SumOfTheAges.java
+++ week1-012.SumOfTheAges/src/SumOfTheAges.java
@@ -1,11 +1,52 @@
+/*
+Exercise #12 SumOfTheAges
+Directions: Create a program that asks for the names and ages of two users.
+After that the program prints the sum of their ages.
 
-import java.util.Scanner;
+Desired output:
+_____________________
+Type your name: Matti
+Type your age: 14
+
+Type your name: Arto
+Type your age: 12
+
+Matti and Arto are 26 years old in total.
+*/
+
+
+
+import java.util.Scanner; //import Scanner class
 
 public class SumOfTheAges {
 
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+    public static void main (String[] args){
 
-        // Implement your program here
+       Scanner reader = new Scanner (System.in); //calls Scanner class into main method of program SumOfTheAges
+
+            System.out.print("Type your name: "); //asking user #1 string prompt
+
+            String firstName = reader.nextLine(); //parses string input; firstName
+
+            System.out.print("Type your age: ");
+
+            int firstAge = Integer.parseInt(reader.nextLine());
+
+            System.out.println(""); //print blank space after name and age of first person
+
+                System.out.print("Type your name: "); //asking user #2 string prompt
+
+                String secondName = reader.nextLine(); //parses string input; secondName
+
+                System.out.print("Type your age: ");
+
+                int secondAge = Integer.parseInt(reader.nextLine());
+
+                System.out.println(""); //print blank space after name and age of second person
+
+        int totalAge = firstAge + secondAge;  //declare int totalAge as sum of ages together to output
+
+        System.out.print(firstName + " and " + secondName + " are " + totalAge + " years old in total."); //concatenation of int vars and string for full total in names and ages.
     }
+
 }
